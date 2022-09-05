a3291c3cf8ae4220058a0bf1aa10a75e6fcc76d0
Tiago Nascimento Moreira <tiago1nasciment@gmail.com>
2022-09-05 17:07:26 -0300

    Update tipo_float.py

--- Variaveis e Tipo de dados/tipo_float.py
+++ Variaveis e Tipo de dados/tipo_float.py
@@ -1,3 +1,23 @@
 """
 Tipo Float
+
+Tipo real, decimal
+
+Casas decimais
+
+OBS: O separador de casas decimais na programação é o ponto e não a virgula.
 """
+
+# Errado do ponto de vista do Float, mas gera uma tupla.
+valor = 1, 44
+print(valor)
+print(type(valor))
+# Certo do ponto de vista Float.
+valor = 1.44
+print(valor)
+print(type(valor))
+
+# É possivel fazer dupla atribuição
+valor1, valor2 = 1, 44
+print(valor1)
+print(valor2)
