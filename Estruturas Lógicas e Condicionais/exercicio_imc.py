431ac1fa396e2ce7d38cd258bb04320c687248d2
Tiago Nascimento Moreira <tiago1nasciment@gmail.com>
2022-09-13 15:00:16 -0300

    Atualização exercicios

--- Estruturas Lógicas e Condicionais/exercicio_imc.py
+++ Estruturas Lógicas e Condicionais/exercicio_imc.py
@@ -7,20 +7,18 @@ def IMC():
 
     imc = peso / (altura * altura)
 
-    print(imc)
-
     if imc < 18.5:
-        print(f'O seu IMC é {imc}. Abaixo do peso.')
+        print(f'O seu IMC é {imc:.2f}. Abaixo do peso.')
     elif imc <= 24.9:
-        print(f'O seu IMC é {imc}. Paudável')
+        print(f'O seu IMC é {imc:.2f}. Saudavel')
     elif imc <= 30:
-        print(f'O seu IMC é {imc}. Peso em excesso')
+        print(f'O seu IMC é {imc:.2f}. Peso em excesso')
     elif imc <= 34.9:
-        print(f'O seu IMC é {imc}. Obesidade Grau 1')
+        print(f'O seu IMC é {imc:.2f}. Obesidade Grau 1')
     elif imc <= 39:
-        print(f'O seu IMC é {imc}. Obesidade Grau 2 (Severa)')
+        print(f'O seu IMC é {imc:.2f}. Obesidade Grau 2 (Severa)')
     elif imc > 40:
-        print(f'O seu IMC é {imc}. Obesidade Grau 3 (Mórbida)')
+        print(f'O seu IMC é {imc:.2f}. Obesidade Grau 3 (Mórbida)')
     else:
         print('Insira corretamente o numero'), IMC()
 
