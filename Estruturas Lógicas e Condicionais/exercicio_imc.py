686c369eeb5fb3e53576420ab558829288f5abfa
Tiago Nascimento Moreira <tiago1nasciment@gmail.com>
2022-09-14 15:18:14 -0300

    Update exercicio_imc.py

--- Estruturas Lógicas e Condicionais/exercicio_imc.py
+++ Estruturas Lógicas e Condicionais/exercicio_imc.py
@@ -23,4 +23,23 @@ def IMC():
         print('Insira corretamente o numero'), IMC()
 
 
-IMC()
+# IMC()
+
+# Exercicio login e senha com estrutura logica
+
+
+def login():
+
+    usuario = input('Nome de usuário: ')
+    senha = input('Senha do usuário :')
+
+    usuario_bd = 'tiago'
+    senha_bd = '123'
+
+    if usuario_bd == usuario and senha_bd == senha:
+        print('Você está logado no sistema')
+    else:
+        print('Usuário ou senha inválidos.')
+
+
+login()
